bbf6e17fbdf8d9daa56a9f593ba23ad5b8630bfd
Talal Tayyab <talal.tayyab@readify.net>
2021-08-28 15:18:37 +1000

    Add Razor application

---
+++ kchat.UI/Framework/MemoryStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kchat.UI.Framework
+{
+    public class MemoryStore
+    {
+        public ConcurrentDictionary<string, string> _keyValuePairs = new ConcurrentDictionary<string, string>();
+
+        public void Add(string key, string value)
+        {
+            _keyValuePairs.AddOrUpdate(key, value, (key, oldvalue) => value);
+        }
+
+        public string Get(string key)
+        {
+            return _keyValuePairs.GetValueOrDefault(key);
+        }
+    }
+}
