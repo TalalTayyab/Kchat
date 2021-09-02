f83ffbf93452f69f6fc15678c49aaf4fcc60506b
Talal Tayyab <talal.tayyab@readify.net>
2021-09-02 22:13:42 +1000

    added dbup migration project

---
+++ kchat.dbUp.migrations/Program.cs
@@ -0,0 +1,42 @@
+﻿using DbUp;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace kchat.dbUp.migrations
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("starting migrations...");
+
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+            connectionString = connectionString ?? @"server=localhost;database=db;trusted_connection=true;";
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+    }
+}
---
+++ kchat.dbUp.migrations/Scripts/Script0001-Create-tables.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Message](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Text] [varchar](max) NOT NULL,
+	[DateTimeOffSet] [datetimeoffset](7) NOT NULL,
+	[UserId] [varchar](200) NOT NULL,
+	[UniqueMessageId] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_Message] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
